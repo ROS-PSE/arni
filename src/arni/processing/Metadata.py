eaed155ba60c8c4207638ce29951b18d5256d1d0
Matthias Klatte <p.roni@proni.net>
2014-07-01 17:01:34 +0200

    Processing classes, added windows version makefile

---
+++ src/arni/processing/Metadata.py
@@ -0,0 +1,34 @@
+from MetadataTuple import MetadataTuple
+
+class Metadata:
+    """
+    Wraps metadata of exactly one host or node, a topic or a node-topic-combination.
+    """
+
+    __values = {}
+
+    def add_tuple(self, tuple):
+        """
+        Adds a MetadataTuple to the bundle.
+        :param tuple: The Tuple to be added.
+        :type tuple: MetadataTuple
+        """
+        self.__values[tuple.key] = tuple
+
+    def get(self, key):
+        """
+        Returns the value of the MetadataTuple with the given key.
+        :param key: The identifier for the stored measurement.
+        :type key: str
+        :return: A value of any type for the measured field. False, if the key does not exist.
+        """
+        if key in self.__values:
+            return self.__values[key]
+        return False
+
+    def __init__(self, tuples = {}):
+        """
+        :param tuples: Optionally give tuples on creation. Use add_tuple(MetadataTuple) afterwards.
+        :type tuples: dict(str, MetadataTuple)
+        """
+        self.__values = tuples
---
+++ src/arni/processing/Specification.py
@@ -0,0 +1,36 @@
+from MetadataTuple import MetadataTuple
+
+class Specification:
+    """
+    An object loaded from the specification configurations
+    and basis for comparison of Metadata
+    with desired values.
+    """
+
+    __values = {}
+
+    def add_tuple(self, tuple):
+        """
+        Adds a MetadataTuple to the bundle.
+        :param tuple: The Tuple to be added.
+        :type tuple: MetadataTuple
+        """
+        self.__values[tuple.key] = tuple
+
+    def get(self, key):
+        """
+        Returns the value of the MetadataTuple with the given key.
+        :param key: The identifier for the stored measurement.
+        :type key: str
+        :return: A list containing limit values for the measured fields. False, if the key does not exist.
+        """
+        if key in self.__values:
+            return self.__values[key]
+        return False
+
+    def __init__(self, tuples = {}):
+        """
+        :param tuples: Optionally give tuples on creation. Use add_tuple(MetadataTuple) afterwards.
+        :type tuples: dict(str, MetadataTuple)
+        """
+        self.__values = tuples
