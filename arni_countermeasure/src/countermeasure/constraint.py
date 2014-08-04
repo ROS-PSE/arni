5035c8b883b8f000f74bb00e64e7d90e5fb32187
gmwe <michawetzel@gmx.de>
2014-08-04 19:40:13 +0200

    Implementation of Constraint (untested).

--- arni_countermeasure/src/countermeasure/constraint.py
+++ arni_countermeasure/src/countermeasure/constraint.py
@@ -1,27 +1,80 @@
+from rated_statistic_storage import *
+from ConstraintItem import *
+
+
 class Constraint(object):
 
-    """Manages all constraints,
-    checks if they are true and executes
-    appropriate reactions if neccessary.
+    """Contains the whole constraint with corresponding reactions.
     """
 
-    def __init__(self):
+    def __init__(
+            self, constraint_root, planned_reaction,
+            min_reaction_interval, reaction_timeout):
         super(Constraint, self).__init__()
 
-        self.__constraint
+        #: The root of the constraint tree.
+        #: :type:   ConstraintItem
+        self.__constraint_root = constraint_root
 
-        self.true_since
+        #: Time since when this constraint is true.
+        #: # Note if true_since is 0 it has not been true.
+        #: :type:   rospy.Time
+        self.true_since = rospy.Time(0)
 
-        self.planned_reaction
+        #: An list of reactions that should be executed if the constraint
+        #: has been true longer than min_reaction_interval milliseconds.
+        #: :type:   list of Reactions
+        self.planned_reaction = planned_reaction
 
-        self.__min_reaction_interval
+        #: The minimum time needed that the constraint needs to be true to
+        #: execute the planned reactions.
+        #: :type:   rospy.Duration
+        self.__min_reaction_interval = min_reaction_interval
 
-        self.__last_reaction
+        #: The time this reaction has been executed for the last time.
+        #: 0 if it has never been executed.
+        #: :type:   rospy.Time
+        self.__last_reaction = rospy.Time(0)
 
-        self.__reaction_timeout
+        #: Minimum durotation needed before an reaction can happen again.
+        #: :type: rospy.Duration
+        self.__reaction_timeout = reaction_timeout
+
+        #: True if the current state of the constraint says that
+        #: an execution of the reactions is necessary.
+        self.evaluation_result = False
 
     def evaluate_constraint(self, storage):
         """Evaluates this constraint and sets the attributes
         according to the result of the evaluation.
+
+        :param storage: The storage where the incoming statistics are saved.
+        :type storage:  RatedStatisticStorage
         """
-        pass
+        evaluation = self.__constraint_root.evaluate_constraint(storage)
+
+        #If the constraint is false only true since has to be reset.
+        if not evaluation:
+            self.true_since = rospy.Time(0)
+            self.evaluation_result = False
+        else:
+            if self.true_since == 0:
+                self.true_since = rospy.Time.now()
+
+            if (
+                (rospy.Time.now - self.true_since
+                    >= self.__min_reaction_interval)
+                and
+                (rospy.Time.now - self.__last_reaction
+                    >= self.__reaction_timeout)):
+                self.evaluation_result = True
+        return
+
+    def notify_of_execution(self):
+        """ Tells this constraint that its reactions have just been executed.
+
+        # Note: its important to notify this constraint so it knows how long
+        the last execution was ago and does not execute too often.
+        """
+        self.evaluation_result = False
+        self.__last_reaction = rospy.Time.now()
