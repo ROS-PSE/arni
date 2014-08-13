f839b8d13b65a9505c2143eec857d96eb6238520
Grommetox <sebastian.kneipp@gmx.net>
2014-08-13 16:38:00 +0200

    added structure

---
+++ arni_gui/AbstractItem.py
@@ -0,0 +1,84 @@
+class AbstractItem:
+""" Provides a unified interface to access the items of the model"""
+
+    def __init__(self, list, parent=None):
+	"""Initializes theAbstractItem
+
+	:param list: item list
+	:type list: list
+	:param parent: the parent-object
+	:type parent: object
+	"""
+	pass
+
+    def append_child(self, child):
+	"""Append a child to the list of childs
+
+	:param child: the child item
+	:type child: AbstractItem
+	"""
+	pass
+
+    def append_data(self, data):
+	"""Append data to the data_list of the AbstractItem
+
+	:param data: the data to appen
+	:type data: object
+	"""
+	pass
+
+    def get_child(self, row):
+	"""Returns the child at the position row
+
+	:param row: the index of the row
+	:type row: int
+	
+	:returns: AbstractItem
+	"""
+	pass
+
+    def get_latest_data(self):
+	"""Returns the latest dict of the data_list
+
+	:returns: dict
+	"""
+	pass
+
+    def parent(self):
+	"""Returns the parent of this or None if there is no parent
+
+	:returns: AbstractItem
+	"""
+	pass
+
+    def get_items_older_than(self, time):
+	"""Returns all items which are older than time
+
+	:param time: the upper bound
+	:type time: rospy.Time
+
+	:returns: AbstractItem
+	"""
+	pass
+
+    def delete_items_older_than(self, time):
+	"""Deletes all items which are older than time
+
+	:param time: the upper bound
+	:type time: rospy.Time
+	"""
+	pass
+
+    def get_items_younger_than(self, time):
+	"""Returns all items which are younger than time
+
+	:param time: the lower bound
+	:type time: rospy.Time
+
+	:returns: AbstractItem
+	"""
+	pass
+
+    @abstractmethod
+    def execute_action(self, action):
+	"""Executes a action on the current item like stop or restart. Calls to this method should be redirected to the remote host on executed there."""
---
+++ arni_gui/LogFilterProxy.py
@@ -0,0 +1,42 @@
+class LogFilterProxy(object):
+"""The LogFilterProxy will espacially be used to filter the complete log e.g. by a specific node. This function is needed in the SelectionWidget where of course only the log of the current selection should be shown."""
+
+    def __init__(self, parent):
+	"""Initializes the LogFilterProxy
+
+	:param parent: the parent-object
+	:type parent: QObject
+	"""
+	pass
+
+    def filterAcceptsRow(self, source_row, source_parent):
+	"""Tells by analysing the given row if it should be shown or not. This behaviour can be modified via setFilterRegExp mehod so that e.g. only the entries of a specific host can be shown.
+
+	:param source_row: the source row
+	:type source_row: int
+	:param source_parent: he source of the parent
+	:type source_parent: QModellIndex
+
+	:returns: bool
+	"""
+	pass
+
+    def lessThan(self, left, right):
+	"""Defines the sorting of behaviour when comparing two entries of model item by telling how to compare these.
+
+	:param left: the left-hand side
+	:type left: QModellIndex
+	:param right: the right-hand side
+	:type right: QModellIndex
+
+	:returns: bool
+	"""
+	pass
+
+    def filter_by_item(self, item):
+	"""Used to tell the filter by which item should filter. If the AbstractItem is None all log entries should be shown.
+
+	:param item: the item by which the filter should filter
+	:type item: AbstractItem
+	"""
+	pass
