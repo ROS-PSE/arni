db2dd20a56aaba68eb2f773d3c4c8b2b8fb54b6e
virtubuntu 14.04 <andreas.bihlmaier@gmx.de>
2014-08-14 23:57:33 +0200

    Implemented ItemFilterProxy and SizeDelegate

--- arni_gui/SizeDelegate.py
+++ arni_gui/SizeDelegate.py
@@ -1,24 +1,69 @@
-#TODO: imports
+"""
+Would probably already work.
+The todos should be fixed earlier or later.
+"""
+from python_qt_binding.QtGui import QStyledItemDelegate, QFont, QBrush, QColor
 
-class SizeDelegate:QtGui.QStyledItemDelegate(object):
-"""Makes it possible to change the font size of the Gui-Plugin content."""
- 
-    def paint(self, painter, option, index):
-	"""Defines how the items of the model will be painted in the view. Can be used  to draw e.g. bigger or smaller fonts
 
-	:param painter:
-	:type painter: QPainter
-	:param option:
-	:type param: QStyleOptionViewItem
-	:param index:
-	:type index: QModellIndex
-	"""
-	pass
+class SizeDelegate(QStyledItemDelegate):
+    """Makes it possible to change the font size of the Gui-Plugin content."""
 
-    def set_bigger_font_size(self):
-	"""Increases the displayed font-size"""
-	pass
 
-    def set_smaller_font_size(self):
-	"""Decreases the displayed font-size"""
-	pass
+def __init__(self, parent=None):
+    QStyledItemDelegate.__init__(parent)
+    self.__current_font_size = 10
+
+
+def paint(self, painter, option, index):
+    """Defines how the items of the model will be painted in the view. Can be used  to draw e.g. bigger or smaller fonts.
+
+    :param painter: The painter which will be used to paint
+    :type painter: QPainter
+    :param option: The options parameter
+    :type option: QStyleOptionViewItem
+    :param index: The QModelIndex that will be painted
+    :type index: QModelIndex
+    """
+    painter.save()
+    # todo:does this has to be a self.font?
+    font = QFont()
+    #setting the size of the font
+    #todo:does this have to be set in the option param?
+    font.setPixelSize(self.__current_font_size)
+
+    brush = __choose_brush(index)
+    painter.setFont(font)
+    painter.setBrush(brush)
+
+    #todo:set painter background for error cases?
+    QStyledItemDelegate.paint(self, painter, option, index)
+
+    painter.restore()
+
+
+def set_bigger_font_size(self):
+    """Increases the displayed font-size."""
+    self.__current_font_size += 2
+
+
+def set_smaller_font_size(self):
+    """Decreases the displayed font-size."""
+    self.__current_font_size -= 2
+
+
+def __choose_brush(index):
+    """
+
+    :type index: QModelIndex
+    """
+    brush = QBrush()
+    # todo:can we assume this is always a string?
+    #todo: add further keywords etc and check for errors. Is this the best possible way?
+    if index.internalPointer().find("ok"):
+        brush.setColor(QColor(127, 255, 0))
+    elif index.internalPointer().find("warning"):
+        brush.setColor(QColor(255, 165, 0))
+    elif index.internalPointer().find("error"):
+        brush.setColor(QColor(255, 0, 0))
+
+    return brush
--- rqt_overview/src/rqt_overview/ros_overview.py
+++ rqt_overview/src/rqt_overview/ros_overview.py
@@ -2,25 +2,25 @@ import os
 import rospy
 import rospkg
 
-
 from python_qt_binding import loadUi
 from python_qt_binding.QtGui import QWidget
 
 from rqt_gui_py.plugin import Plugin
 
-class OverviewPlugin(Plugin):
 
+class OverviewPlugin(Plugin):
     def __init__(self, context):
         super(OverviewPlugin, self).__init__(context)
         self.setObjectName('OverviewPlugin')
 
         # Process standalone plugin command-line arguments
         from argparse import ArgumentParser
+
         parser = ArgumentParser()
         # Add argument(s) to the parser.
         parser.add_argument("-q", "--quiet", action="store_true",
-                      dest="quiet",
-                      help="Put plugin in silent mode")
+                            dest="quiet",
+                            help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         if not args.quiet:
             print 'arguments: ', args
@@ -30,7 +30,7 @@ class OverviewPlugin(Plugin):
         self._widget = QWidget()
         # Get path to UI file which is a sibling of this file
         # in this example the .ui and .py file are in the same folder
-	rp = rospkg.RosPack()
+        rp = rospkg.RosPack()
         ui_file = os.path.join(rp.get_path('rqt_overview'), 'resources', 'OverviewWidget.ui')
         # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
@@ -45,36 +45,36 @@ class OverviewPlugin(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
-	self._connect_slots()
+        self._connect_slots()
 
     def _connect_slots(self):
-	"""Initializes the slots of the OverviewPlugin."""
-	self._widget.tab_widget.currentChanged.connect(self._on_current_tab_changed)
-	self._widget.range_combo_box.currentIndexChanged.connect(self.on_range_combo_box_index_changed)
+        """Initializes the slots of the OverviewPlugin."""
+        self._widget.tab_widget.currentChanged.connect(self._on_current_tab_changed)
+        self._widget.range_combo_box.currentIndexChanged.connect(self.on_range_combo_box_index_changed)
 
     def _on_current_tab_changed(self, tab):
-	"""The Plugin wants to get notified when the tab changed so it can e.g. draw the graphs.
+        """The Plugin wants to get notified when the tab changed so it can e.g. draw the graphs.
 
 	:param tab: the index of the selected tab
 	:type tab: int
 	"""
-	pass
+        pass
 
     def on_range_combo_box_index_changed(self, index):
-	"""Handels the change of the graph range.
+        """Handels the change of the graph range.
 	
 	:param index: the index of the selected range
 	:type index: int
 	"""
-	pass	
+        pass
 
     def update(self):
-	"""Updates the Plugin and draws the graphs if draw_graphs is true."""
-	pass
+        """Updates the Plugin and draws the graphs if draw_graphs is true."""
+        pass
 
     def update_graphs(sef):
-	"""Updates and redraws the graphs"""
-	pass
+        """Updates and redraws the graphs"""
+        pass
 
     def shutdown_plugin(self):
         # TODO unregister all publishers here
@@ -90,7 +90,7 @@ class OverviewPlugin(Plugin):
         # v = instance_settings.value(k)
         pass
 
-    #def trigger_configuration(self):
+        # def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
         # This will enable a setting button (gear icon) in each dock widget title bar
         # Usually used to open a modal configuration dialog
