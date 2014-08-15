8067e0e08eacb34ab25ecb5eb0cbf0aa0eee4586
Matthias Klatte <p.roni@proni.net>
2014-08-15 17:17:46 +0200

    Specifications get loaded from /arni/specifications, specification format adjusted, see changes.md

--- arni_core/src/arni_core/helper.py
+++ arni_core/src/arni_core/helper.py
@@ -1,3 +1,5 @@
+import re
+
 """
 This helper module contains useful functions and variables that
 are needed throughout the whole arni package.
@@ -5,3 +7,19 @@ are needed throughout the whole arni package.
 
 #: the delimiter the seuid uses
 SEUID_DELIMITER = "!"
+
+def is_seuid(seuid):
+    """
+    Determines whether the given parameter is a valid seuid.
+    :param seuid: A string presenting a seuid to validate.
+    :return: True, if the given parameter is a valid seuid, false if not.
+    """
+    p = re.compile('^([nhtc])' + SEUID_DELIMITER + '([_a-z].*)')
+    m = p.match(seuid)
+    if m == None:
+        return False
+    arglen = len(m.group(2).strip().split(SEUID_DELIMITER))
+    if m.group(1) == "c":
+        return arglen == 3
+    else:
+        return arglen == 1
