57f9c7e2ea31e527b2b3c76a7be1ae5fb5dc3837
cinexw <alex.weber3@gmx.net>
2014-08-25 14:10:11 +0200

    nodeinterface should be runnable now

--- arni_nodeinterface/src/arni_nodeinterface/run_node.py
+++ arni_nodeinterface/src/arni_nodeinterface/run_node.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python
+!#/usr/bin/env python
 import rospy
-from host_statistics_handler import *
+from host_statistics_handler import HostStatisticsHandler
 import os
 
 def main():
-    
-    #howto ROS_IP ?
+
     ip = os.getenv('ROS_IP', '127.0.0.1')
-    ip = ip.replace('.','_')
+    ip = ip.replace('.', '_')
 
-    rospy.init_node("HostStatistics_%s"%ip, log_level=rospy.DEBUG)
+    rospy.init_node('HostStatistics_%s'%ip, log_level=rospy.DEBUG)
     rospy.sleep(1)
-
-
     host = HostStatisticsHandler(ip)
-
     try:
         rospy.sleep(1)
-        rospy.Timer(rospy.Duration(host.update_intervall), host.measure_status)
+        rospy.Timer(rospy.Duration(host.update_intervall),host.measure_status)
         rospy.Timer(rospy.Duration(host.publish_intervall),host.publish_status)
         while not rospy.is_shutdown():
             rospy.spin()
-        #rospy.on_shutdown(host.shut_down_hook)
     except rospy.ROSInterruptException:
         pass
-
 
 if __name__ == '__main__' : main()
