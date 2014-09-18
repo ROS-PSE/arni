8e1c4dda2acd8fb7bc116c8e3bbc7808dca2c634
gmwe <michawetzel@gmx.de>
2014-09-18 18:44:41 +0200

    working implementation of sine function.

--- arni_core/test/predefined_publisher.py
+++ arni_core/test/predefined_publisher.py
@@ -9,72 +9,79 @@ import array
 
 pub = None
 
-def floored_abs():
-    counter = 1
-    step_size = 5000
-    maximum = 20000
-    distance = maximum - counter
-    duration = rospy.get_param("~duration", 10)
-    time_step = distance / duration
-    direction = 1
-    while not rospy.is_shutdown():
-        c = math.floor(counter / step_size) + 1
-        counter += direction * time_step
-        if counter >= maximum:
-            direction = -1
-        if direction < 0 and counter == 1:
-            direction = 1
-        c *= 1000
-        print(c)
-        pub.publish(
-            ("%s times, " % int(c)) + "publishing with floored_abs. " * int(c))
-        rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
+# not truly implemented yet.
+# def floored_abs():
+#     counter = 1
+#     step_size = 5000
+#     maximum = 20000
+#     distance = maximum - counter
+#     duration = rospy.get_param("~duration", 10)
+#     time_step = distance / duration
+#     direction = 1
+#     while not rospy.is_shutdown():
+#         c = math.floor(counter / step_size) + 1
+#         counter += direction * time_step
+#         if counter >= maximum:
+#             direction = -1
+#         if direction < 0 and counter == 1:
+#             direction = 1
+#         c *= 1000
+#         print(c)
+#         pub.publish(
+#             ("%s times, " % int(c)) + "publishing with floored_abs. " * int(c))
+#         rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
 
 
-def linear_abs():
-    counter = 1
-    maximum = 20000
-    distance = maximum - counter
-    duration = rospy.get_param("~duration", 10)
-    time_step = distance / duration
-    direction = 1
-    while not rospy.is_shutdown():
-        counter += direction * time_step
-        print(counter)
-        if counter >= maximum:
-            direction = -1
-        if direction < 0 and counter == 1:
-            direction = 1
-        pub.publish(("%s times, " % int(math.floor(counter))) +
-                    "publishing with linear_abs. " * int(math.floor(counter)))
-        rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
+# def linear_abs():
+#     counter = 1
+#     maximum = 20000
+#     distance = maximum - counter
+#     duration = rospy.get_param("~duration", 10)
+#     time_step = distance / duration
+#     direction = 1
+#     while not rospy.is_shutdown():
+#         counter += direction * time_step
+#         print(counter)
+#         if counter >= maximum:
+#             direction = -1
+#         if direction < 0 and counter == 1:
+#             direction = 1
+#         pub.publish(("%s times, " % int(math.floor(counter))) +
+#                     "publishing with linear_abs. " * int(math.floor(counter)))
+#         rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
 
 
-def sawtooth():
-    high = rospy.get_param("~bandwidth_high", 200)
-    low = rospy.get_param("~bandwidth_low", 1)
-    distance = high - low
-    steps = rospy.get_param("~steps", distance)
-    step = distance / steps
-    counter = 1
-    while not rospy.is_shutdown():
-        pub.publish("publishing with sawtooth. " * int(counter))
-        counter += step
-        if counter >= high:
-            counter = 1
-        rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
+# def sawtooth():
+#     high = rospy.get_param("~bandwidth_high", 200)
+#     low = rospy.get_param("~bandwidth_low", 1)
+#     distance = high - low
+#     steps = rospy.get_param("~steps", distance)
+#     step = distance / steps
+#     counter = 1
+#     while not rospy.is_shutdown():
+#         pub.publish("publishing with sawtooth. " * int(counter))
+#         counter += step
+#         if counter >= high:
+#             counter = 1
+#         rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
 
 
 def sine():
-    mid = rospy.get_param("~bandwidth_mid", 100)
-    var = rospy.get_param("~bandwidth_variation", 5000)
-    counter = 0
+    frequency = rospy.get_param("~frequency", 10)
+    rate = rospy.Rate(frequency)
+
+    mid = rospy.get_param("~bandwidth_mid", 1024) / frequency
+    var = rospy.get_param("~bandwidth_variation", 500) / frequency
+    # period normalised to 
+    period = rospy.get_param("~period", 1)
+    begin = rospy.Time.now()
+
     while not rospy.is_shutdown():
-        c = math.sin(counter / 180 * math.pi)
-        v = mid + c * var
-        pub.publish("publishing with sine. " * int(v))
-        counter += 1
-        rospy.Rate(rospy.get_param("~frequency", 10)).sleep()
+        cur = rospy.Time.now() - begin
+        fluctuation = math.sin(2 * math.pi / period * cur.to_sec()) * var
+        msgsize = mid + fluctuation
+        pub.publish("." * int(msgsize))
+        rate.sleep()
 
 
 def constant():
@@ -143,14 +150,15 @@ linear_abs:
 '''
 if __name__ == '__main__':
     rospy.init_node('node_name', log_level=rospy.DEBUG)
-    pub = rospy.Publisher(rospy.get_param('~topic_name', 'topic_name'), String, queue_size=10)
+    pub = rospy.Publisher(
+        rospy.get_param('~topic_name', 'topic_name'), String, queue_size=10)
 
     modes = {
         'constant': constant,
         'stop_publish': stop_publish,
-        'sawtooth': sawtooth,
+        # 'sawtooth': sawtooth,
         'sine': sine,
         'high_low': high_low}
-    mode = rospy.get_param("~mode", "high_low")
+    mode = rospy.get_param("~mode", "sine")
     if mode in modes:
         modes[mode]()
